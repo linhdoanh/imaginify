9be5006976e90590fe0ec3bb217a359f1835481e
anhlinhkbsea <anhlinh.do@kirby.vn>
2024-04-15 11:48:59 +0700

    fix route clerk

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -67,9 +67,9 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username,
-      firstname: first_name,
-      lastname: last_name,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
       photo: image_url,
     }
 
@@ -92,9 +92,9 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstname: first_name,
-      lastname: last_name,
-      username: username,
+      firstName: first_name,
+      lastName: last_name,
+      username: username!,
       photo: image_url,
     }
 
